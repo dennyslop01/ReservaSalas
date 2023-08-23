437f31a9de2531b0ac65d3e026876ca62add03a1
dennys.lopez <dennys.lopez@blackfincorp.com>
2023-08-23 09:25:54 -0400

    Ajustes para continuar el proyecto

--- ReservaSalas/urls.py
+++ ReservaSalas/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('client/', include('client.urls')),
-    path('room/', include('room.urls'))
+    path('room/', include('room.urls')),
+    path('', views.index, name='index')
 ]
--- client/views.py
+++ client/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Client
 
-# Create your views here.
+def index(request):
+    clients = Client.objects.filter(name__contains=request.GET.get('search', ''))
+    
+    context = {
+        'clients': clients
+    }
+    return render(request, 'client/index.html', context)
+
+def view(request, id):
+    client = Client.objects.get(id=id)
+    context = { 'client': client}
+    return render(request, 'client/detail.html', context)
